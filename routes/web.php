2d2d2dcbb237cc160fcf8b66c4c513b6a1f4ded5
andaaree <andaaree@gmail.com>
2022-01-13 14:58:21 +0700

    init

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,29 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', 'HomeController@index');
+
+//Admin Back-End
+Route::get('admin', 'Admin@index');
+Route::middleware('auth')->group(function () {
+    Route::prefix('admin')->group(function () {
+        Route::resource('buku', 'BookController');
+        Route::resource('user', 'UserController');
+        Route::resource('kelas', 'GradeController');
+        Route::resource('video', 'VideoController');
+        Route::resource('jurusan', 'MajorController');
+        Route::resource('mapel', 'SubjectController');
+        Route::resource('sekolah', 'SchoolController');
+        Route::resource('riwayat', 'HistoryController');
+        Route::resource('pendidikan', 'EducationController');
+        Route::post('user/storeOne', 'UserController@storeOne');
+        Route::get('pendidikan/all', 'EducationController@getjson');
+    });
+    Route::get('riwayat/{user}', 'HistoryController@show');
+    Route::get('profile/{user}', 'UserController@profile');
 });
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
+Auth::routes([
+    'register' => false, // Registration Routes...
+    // 'reset' => false, // Password Reset Routes...
+    'verify' => false, // Email Verification Routes...
+]);
