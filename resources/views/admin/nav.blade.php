2361fbe6839b7e507e1435a6d9c4e9f200514130
dianindahfajrianti <dianindahfajrianti@gmail.com>
2022-02-08 15:38:47 +0700

    pdf

--- resources/views/admin/nav.blade.php
+++ resources/views/admin/nav.blade.php
@@ -11,7 +11,8 @@
         <!-- Sidebar user panel (optional) -->
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
             <div class="image">
-                <img src="/assets/adminlte/dist/img/" class="img-circle elevation-2" alt="User Image">
+                <img src="\assets\perpus\assets\img\profile-img.png" class="img-circle" alt="">
+                <!-- <img src="/assets/adminlte/dist/img/" class="img-circle elevation-2" alt="User Image"> -->
             </div>
             <div class="info">
                 <a href="{{url('/admin')}}" class="d-block">{{Auth::user()->name}}</a>
