890a248ab3c0fb809a89bb9f969ade33c6fd0187
dianindahfajrianti <dianindahfajrianti@gmail.com>
2022-07-22 09:05:33 +0700

    update breadcrumbs akses

--- resources/views/permission/filesekolah.blade.php
+++ resources/views/permission/filesekolah.blade.php
@@ -17,6 +17,7 @@
             <div class="col-sm-6">
                 <ol class="breadcrumb float-sm-right">
                     <li class="breadcrumb-item"><a href="/admin">Home</a></li>
+                    <li class="breadcrumb-item"><a href="/admin/akses">Akses</a></li>
                     <li class="breadcrumb-item active">Sekolah</li>
                 </ol>
             </div>
