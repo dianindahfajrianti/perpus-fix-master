0c65b9a70990bb403c6fb7d2e2f6c59aa2c2a23e
dianindahfajrianti <dianindahfajrianti@gmail.com>
2022-07-28 10:05:14 +0700

    update old kelas dan sekolah tambah user

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -353,6 +353,30 @@
             });
         });
 
+        var sekolahID = $('#sekolah').val();
+        var oldKelas = "{{ old('kelas') }}"
+        console.log(sekolahID);
+        if(sekolahID) {
+            $.ajax({
+                url: '/sch/'+sekolahID,
+                type: "GET",
+                success:function(data){
+                    console.log(data);
+                    $('#kelas').empty();
+                    $('#kelas').append('<option hidden>-- Pilih Kelas --</option>'); 
+                    $.each(data, function(id, kelas){
+                        if (oldKelas = kelas.id){
+                            $('select[name="kelas"]').append('<option selected value="'+ id +'">' + kelas.grade_name+ '</option>');
+                        }else{
+                            $('select[name="kelas"]').append('<option value="'+ id +'">' + kelas.grade_name+ '</option>');
+                        }
+                    });
+                }
+            });
+        }else{
+            $('#kelas').empty();
+        }
+
         $('#sekolah').on('change', function() {
             var sekolahID = $(this).val();
             console.log(sekolahID);
