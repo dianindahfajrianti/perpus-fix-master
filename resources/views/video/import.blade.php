0bbabb62506308be9271fa65f849879ed86e7632
dianindahfajrianti <dianindahfajrianti@gmail.com>
2022-08-08 09:45:03 +0700

    update progress bar import

--- resources/views/video/import.blade.php
+++ resources/views/video/import.blade.php
@@ -66,14 +66,31 @@
                                             <span class="input-group-text" id="ct-file-desc">Upload</span>
                                         </div>
                                     </div>
+                                    <div id="totalFiles">Total : <span class="totalFiles"></span> File(s) </div>
                                 </div>
                                 
                                 <div class="form-group">
-                                    <a href="" class="form-control" id="your-file"></a>
+                                    {{-- <a href="" class="form-control" id="your-file"></a> --}}
                                     <div class="progress">
-                                        <div class="progress-bar progress-bar-striped progress-bar-animated bg-info" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0%"></div>
+                                        <div class="progress-bar progress-bar-striped progress-bar-animated bg-info" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0%" id="pb_total"></div>
                                     </div>
                                 </div>
+
+                                <div class="form-group">
+                                    <ul class="list-group" id="list-file">
+                                        <li class="list-group-item">
+                                            <div class="row justify-content-center align-items-center text-center text-bold">
+                                                <div class="col-7">File Name</div>
+                                                <div class="col-2">Size</div>
+                                                <div class="col-3">Progress</div>
+                                            </div>
+                                        </li>
+                                    </ul>
+                                    <ul class="list-group" id="list-file">
+                                        
+                                    </ul>
+                                </div>
+
                             </div>
                         </div>
                     </div>
@@ -103,8 +120,11 @@
         })
         bsCustomFileInput.init();
 
-        $('#your-file').hide();
+        // $('#your-file').hide();
+        $('#list-file').hide();
+        $('#totalFiles').hide();
         $('#save-file').addClass('disabled');
+
         var totalFiles = 0;
         var counter = 0;
         let browseFile = $('#ct-file');
@@ -112,6 +132,7 @@
             e.preventDefault();
             var file = $(this)[0].files;
             totalFiles = file.length;
+            // console.log(totalFiles);
         });
         
         let resumable = new Resumable({
@@ -127,40 +148,106 @@
         , testChunks: false
         , throttleProgressCallbacks: 1
         , });
-        
+        var uploaded = 0;
+        var total_filesize = 0;
         resumable.assignBrowse(browseFile);
         var files;
 
         resumable.on('fileAdded', function(file, event) { // trigger when file picked
+            total_filesize += file.size;
+            $('.totalFiles').text(totalFiles);
+            $('#totalFiles').show();
+            
+            let fsize = file.size;
+            if (fsize / 1024 > 1)
+            {
+                if (((fsize / 1024) / 1024) > 1)
+                {
+                    fsize = (Math.round(((fsize / 1024) / 1024) * 100) / 100);
+                    var actual_fileSize = fsize + " GB";
+                }
+                else
+                {
+                    fsize = (Math.round((fsize / 1024) * 100) / 100)
+                    var actual_fileSize = fsize + " MB";
+                }
+            }
+            else
+            {
+                fsize = (Math.round(fsize * 100) / 100)
+                var actual_fileSize = fsize  + " KB";
+            }
 
+            // console.log('Total File Size : ', total_filesize)
+            // alert(fsize);
+            let fname = file.fileName;
+            // console.log('filename : ',fname);
+            console.log(file);
+            let list = $('#list-file');
+            
+            let li = '<li class="list-group-item"><div class="row justify-content-center align-items-center"><div class="col-7">'+fname+'</div><div class="col-2 text-center">'+actual_fileSize+'</div><div class="col-3"><div class="progress"><div class="progress-bar progress-bar-striped progress-bar-animated bg-info" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0%" id="pb'+file.uniqueIdentifier+'"></div></div></div></div></li>';
+            list.show();
+            list.append(li);
+            // $('#fname').text(fname);
+            // $('.fsize').text(fsize);
             showProgress();
-            // let fSize = file.size / 1000000;
-            // alert(fSize);
-            // console.log(file);
             resumable.upload() // to actually start uploading.
         });
         
         resumable.on('fileProgress', function(file) { // trigger when file progress update
             files = resumable.files.length;
+            
             // if(files > 1){
             //     updateProgress(Math.floor((file.progress() * 100) / files));
             // }else{
-                updateProgress(Math.floor(file.progress() * 100));
+                // console.log('progress ',file.progress()*100)
+                
+                let progress = $('#pb'+file.uniqueIdentifier);
+                let value = Math.floor(file.progress() * 100);
+                pb_uploaded = Math.floor((uploaded / total_filesize )*100);
+                // console.log(value);
+                // $('#test'+file.uniqueIdentifier).text(value);
+                $('#pb'+file.uniqueIdentifier).css('width', `${value}%`);
+                $('#pb'+file.uniqueIdentifier).html(`${value}%`);
+                // console.log('Uploaded before ',file)
+                // uploaded = uploaded + file.chunks[0].loaded;
+                //     // console.log('File Chunk',file);
+                // console.log('Uploaded :',uploaded, ' of ', total_filesize);
+                // console.log('pb Uploaded : ', pb_uploaded );
+                console.log("File proggress var : "+file.progress());
+                // let tp = Math.floor((file.progress() + ) * 100);
+
+                // updateProgress(tp);
             // }
         });
 
         resumable.on('fileSuccess', function(file, response) { // trigger when file upload complete
             response = JSON.parse(response)
-            $('#your-file').attr('href', response.path);
-            $('#your-file').text(response.filename);
-            $('#your-file').show();
+            // $('#your-file').attr('href', response.path);
+            // $('#your-file').text(response.filename);
+            // $('#your-file').show();
             counter++;
-            
+
+            // console.log(total_filesize);
+            // uploaded += file.size;
+            //     console.log('File Chunk',file);
+            //     pb_uploaded = (uploaded / total_filesize);
+            uploaded += file.size;
+            // console.log(file);
+            pb_uploaded = (uploaded / total_filesize)*100;
+            // console.log('Uploaded :',uploaded);
+            // console.log('pb uploaded : ',pb_uploaded);
+            $('#pb_total').css('width', `${pb_uploaded}%`);
+            $('#pb_total').html(`${pb_uploaded}%`);
+
             if (counter == totalFiles) {
                 $('#save-file').removeClass('disabled');
             }
-            console.log(counter);
+            // console.log(counter);
         });
+        // resumable.on('complete',()=>{
+        //     $('#save-file').removeClass('disabled');
+        // })
 
         resumable.on('fileError', function(file, response) { // trigger when there is any error
             Swal.fire({
@@ -170,7 +257,6 @@
                 timer: 1800
             });
         });
-
 
         let progress = $('.progress');
 
