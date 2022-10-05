c3f1a76357d008ace018fc177a0c0d6792625e61
dianindahfajrianti <dianindahfajrianti@gmail.com>
2022-10-05 15:00:35 +0700

    Add Favicons

--- resources/views/admin/body.blade.php
+++ resources/views/admin/body.blade.php
@@ -4,6 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <!-- Favicons -->
+    <link href="/assets/perpus/assets/img/perpus.png" rel="icon">
+    <link href="/assets/perpus/assets/img/perpus.png" rel="apple-touch-icon">
+    
     @yield('csrf-ajax')
     @yield('seo')
     <!-- Meta -->
